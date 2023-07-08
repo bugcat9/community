f584bcb59c4828e7b27917fa5bad7078e64f826c
bugcat9 <1767508581@qq.com>
2023-07-08 15:39:36 +0800

    显示评论

---
+++ src/main/java/com/bugcat/community/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.bugcat.community.service;
+
+import com.bugcat.community.dao.CommentMapper;
+import com.bugcat.community.entity.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
+        return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
+    }
+
+    public int findCommentCount(int entityType, int entityId) {
+        return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+}
