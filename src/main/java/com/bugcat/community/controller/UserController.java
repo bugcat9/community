3d3914204c5612c0640816d4a3684a16c5547eca
bugcat9 <1767508581@qq.com>
2023-07-12 23:39:45 +0800

    关注和取关

--- src/main/java/com/bugcat/community/controller/UserController.java
+++ src/main/java/com/bugcat/community/controller/UserController.java
@@ -2,8 +2,10 @@ package com.bugcat.community.controller;
 
 import com.bugcat.community.annotation.LoginRequired;
 import com.bugcat.community.entity.User;
+import com.bugcat.community.service.FollowService;
 import com.bugcat.community.service.UserService;
 import com.bugcat.community.service.LikeService;
+import com.bugcat.community.util.CommunityConstant;
 import com.bugcat.community.util.CommunityUtil;
 import com.bugcat.community.util.HostHolder;
 import org.apache.commons.lang3.StringUtils;
@@ -27,7 +29,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements CommunityConstant {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -48,7 +50,8 @@ public class UserController {
 
     @Autowired
     private LikeService likeService;
-
+    @Autowired
+    private FollowService followService;
     @LoginRequired
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
     public String getSettingPage() {
@@ -140,6 +143,19 @@ public class UserController {
         // 点赞数量
         int likeCount = likeService.findUserLikeCount(userId);
         model.addAttribute("likeCount", likeCount);
+
+        // 关注数量
+        long followeeCount = followService.findFolloweeCount(userId, ENTITY_TYPE_USER);
+        model.addAttribute("followeeCount", followeeCount);
+        // 粉丝数量
+        long followerCount = followService.findFollowerCount(ENTITY_TYPE_USER, userId);
+        model.addAttribute("followerCount", followerCount);
+        // 是否已关注
+        boolean hasFollowed = false;
+        if (hostHolder.getUser() != null) {
+            hasFollowed = followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+        }
+        model.addAttribute("hasFollowed", hasFollowed);
 
         return "/site/profile";
     }
