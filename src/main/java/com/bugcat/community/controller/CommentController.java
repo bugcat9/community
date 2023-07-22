91ef8c295f04559c5d38bcc4e9df3ce1ad1db2fd
bugcat9 <1767508581@qq.com>
2023-07-22 20:28:36 +0800

    热帖排行

--- src/main/java/com/bugcat/community/controller/CommentController.java
+++ src/main/java/com/bugcat/community/controller/CommentController.java
@@ -8,7 +8,9 @@ import com.bugcat.community.service.CommentService;
 import com.bugcat.community.service.DiscussPostService;
 import com.bugcat.community.util.CommunityConstant;
 import com.bugcat.community.util.HostHolder;
+import com.bugcat.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +33,8 @@ public class CommentController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
-
+    @Autowired
+    private RedisTemplate redisTemplate;
     @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
     public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
         comment.setUserId(hostHolder.getUser().getId());
@@ -63,6 +66,9 @@ public class CommentController implements CommunityConstant {
                     .setEntityType(ENTITY_TYPE_POST)
                     .setEntityId(discussPostId);
             eventProducer.fireEvent(event);
+            // 计算帖子分数
+            String redisKey = RedisKeyUtil.getPostScoreKey();
+            redisTemplate.opsForSet().add(redisKey, discussPostId);
         }
 
         return "redirect:/discuss/detail/" + discussPostId;
